65e55bfe3b16e09b30a9e7d09dfe6b6dd60956a4
Sanja Stojanoska <sanja.s.stojanoska@gmail.com>
2021-09-10 23:54:53 +0200

    Add config file

---
+++ utils/config.py
@@ -0,0 +1,6 @@
+
+
+TAG2IDX = {'ADJ': 0, 'ADP': 1, 'ADV': 2, 'AUX': 3, 'CCONJ': 4, 'DET': 5,
+           'INTJ': 6, 'NOUN': 7, 'NUM': 8, 'PART': 9, 'PRON': 10, 'PROPN': 11,
+           'PUNCT': 12, 'SCONJ': 13, 'VERB': 14
+           }
